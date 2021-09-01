2de89f7de3821224c0b9d11686b2d519ea779770
Ian Destura <idestura35.com>
2021-09-01 23:31:09 +0800

    second commit

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-document.getElementById("#c1").addEventListener("click", () => {
+document.getElementById("c1").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Hex-Color-Generator/");
 });
 
-document.getElementById("#c2").addEventListener("click", () => {
+document.getElementById("c2").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Random-Quotes-Generator/");
 });
 
-document.getElementById("#c3").addEventListener("click", () => {
+document.getElementById("c3").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Image-Carousel/");
 });
 
-document.getElementById("#c4").addEventListener("click", () => {
+document.getElementById("c4").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Digital-Clock/");
 });
 
-document.getElementById("#c5").addEventListener("click", () => {
+document.getElementById("c5").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Calculator/");
 });
 
-document.getElementById("#c6").addEventListener("click", () => {
+document.getElementById("c6").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Grocery-List/");
 });
 
-document.getElementById("#c7").addEventListener("click", () => {
+document.getElementById("c7").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Tip-Calculator/");
 });
 
-document.getElementById("#c8").addEventListener("click", () => {
+document.getElementById("c8").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/To-Do-List/");
 });
 
-document.getElementById("#c9").addEventListener("click", () => {
+document.getElementById("c9").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Flashcards/");
 });
 
-document.getElementById("#c10").addEventListener("click", () => {
+document.getElementById("c10").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Sticky-Notes/");
 });
 
-document.getElementById("#c11").addEventListener("click", () => {
+document.getElementById("c11").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Timer/");
 });
 
-document.getElementById("#c12").addEventListener("click", () => {
+document.getElementById("c12").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Kinder-Math/");
 });
 
-document.getElementById("#c13").addEventListener("click", () => {
+document.getElementById("c13").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Capture/");
 });
 
-document.getElementById("#c14").addEventListener("click", () => {
+document.getElementById("c14").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Quotes/");
 });
 
-document.getElementById("#c15").addEventListener("click", () => {
+document.getElementById("c15").addEventListener("click", function() {
         window.open("https://aiyuuu033119.github.io/Square-Cards/");
 });
