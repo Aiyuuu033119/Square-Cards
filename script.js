641c81d60ead3331a0c7c25176f144d2e0ef7562
Ian Destura <idestura35.com>
2021-09-01 23:23:25 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+document.getElementById("#c1").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Hex-Color-Generator/");
+});
+
+document.getElementById("#c2").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Random-Quotes-Generator/");
+});
+
+document.getElementById("#c3").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Image-Carousel/");
+});
+
+document.getElementById("#c4").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Digital-Clock/");
+});
+
+document.getElementById("#c5").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Calculator/");
+});
+
+document.getElementById("#c6").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Grocery-List/");
+});
+
+document.getElementById("#c7").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Tip-Calculator/");
+});
+
+document.getElementById("#c8").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/To-Do-List/");
+});
+
+document.getElementById("#c9").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Flashcards/");
+});
+
+document.getElementById("#c10").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Sticky-Notes/");
+});
+
+document.getElementById("#c11").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Timer/");
+});
+
+document.getElementById("#c12").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Kinder-Math/");
+});
+
+document.getElementById("#c13").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Capture/");
+});
+
+document.getElementById("#c14").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Quotes/");
+});
+
+document.getElementById("#c15").addEventListener("click", () => {
+        window.open("https://aiyuuu033119.github.io/Square-Cards/");
+});
